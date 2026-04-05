795788f6d1dfbe20dcf30f4c595ab8519ce1a8cd
massielcr <massiel.dev.ca@gmail.com>
2026-04-05 17:32:55 -0400

    Books Api

---
+++ src/Books.API/Controllers/BookCollectionsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Books.API.Models;
+using Books.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [Route("api/bookcollections")]
+    [ApiController]
+    public class BookCollectionsController(IMapper mapper, IBooksRepository booksRepository) : Controller
+    {
+        private readonly IMapper _mapper = mapper;
+        private readonly IBooksRepository _booksRepository = booksRepository;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBookCollections(IEnumerable<BookForCreationDto> bookForCreationDtos)
+        {
+            var books = _mapper.Map<IEnumerable<Entities.Book>>(bookForCreationDtos);
+
+            foreach (var book in books)
+            {
+                _booksRepository.AddBook(book);
+            }
+
+            await _booksRepository.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
