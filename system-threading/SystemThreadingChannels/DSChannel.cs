a8e97323fe98671986a42de847041474814fc995
massielcr <massiel.dev.ca@gmail.com>
2026-09-20 12:24:50 -0400

    added implementation for Channel

---
+++ system-threading/SystemThreadingChannels/DSChannel.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Channels;
+
+namespace SystemThreadingChannels
+{
+    internal static class DSChannel
+    {
+        internal static async Task Run(int itemsCount)
+        {
+
+            Channel<int> channel = Channel.CreateUnbounded<int>();
+
+            Task producer = ProduceAsync(channel.Writer, itemsCount);
+            Task consumer = ConsumeAsync(channel.Reader);
+
+
+            static async Task ProduceAsync(ChannelWriter<int> writer, int count)
+            {
+                for(int i = 0; i < count; i++)
+                {
+                    await writer.WriteAsync(i);
+                }
+
+                writer.Complete();
+            }
+
+            static async Task ConsumeAsync(ChannelReader<int> reader)
+            {
+                await foreach(int  item in reader.ReadAllAsync())
+                {
+                    Console.WriteLine($"Received: {item}");
+                }
+
+                Console.WriteLine("done");
+            }
+        }
+    }
+}
---
+++ system-threading/SystemThreadingChannels/Program.cs
@@ -0,0 +1,25 @@
+﻿using SystemThreadingChannels;
+
+class Program
+{
+    static async Task Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("OPTIONS:");
+            Console.WriteLine("[1]  - Channel<T>");
+
+            string? key = Console.ReadLine();
+
+            switch (key)
+            {
+                case "1":
+                    await DSChannel.Run(10);
+                    goto default;
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
